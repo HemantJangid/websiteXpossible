f64cf1b632f42f5a4f6e707ccd0e7ae4643ab115
Hemant Jangid <jhemant539@gmail.com>
2023-07-16 15:28:09 +0530

    changes listed in doc

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ hamburger.addEventListener("click", () => {
 close.addEventListener("click", () => {
   navOverlay.classList.remove("active");
 });
+
+function createElementFromHTML(htmlString) {
+  var div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
+
+  // Change this to div.childNodes to support multiple top-level nodes.
+  return div.firstChild;
+}
